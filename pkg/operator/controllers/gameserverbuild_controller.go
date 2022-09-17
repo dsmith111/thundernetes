cc023f16e68036c367846ddc041d275b2948f08d
dsmith111 <davidcsmith12@gmail.com>
2022-09-16 18:28:26 -0700

    reinstating standby sum

--- pkg/operator/controllers/gameserverbuild_controller.go
+++ pkg/operator/controllers/gameserverbuild_controller.go
@@ -142,8 +142,8 @@ func (r *GameServerBuildReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 	// calculate counts by state so we can update .status accordingly
 	var activeCount, standingByCount, crashesCount, initializingCount, pendingCount int
 	// Gather sum of time taken to reach standingby phase and server count to produce the recent average gameserver initialization time
-	// var timeToStandBySum float64
-	// var recentStandingByCount int
+	var timeToStandBySum float64
+	var recentStandingByCount int
 
 	// Gather current sum of estimated time taken to clean up crashed or pending deletion gameservers
 	var timeToDeleteBySum float64
@@ -158,10 +158,10 @@ func (r *GameServerBuildReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 			initializingCount++
 		} else if gs.Status.State == mpsv1alpha1.GameServerStateStandingBy && gs.Status.Health == mpsv1alpha1.GameServerHealthy {
 			standingByCount++
-			// if gs.Status.State != gs.Status.PrevState {
-			// 	// timeToStandBySum += float64(gs.Status.ReachedStandingByOn.Sub(gs.CreationTimestamp.Time).Milliseconds())
-			// 	// recentStandingByCount++
-			// }
+			if gs.Status.State != gs.Status.PrevState {
+				timeToStandBySum += float64(gs.Status.ReachedStandingByOn.Sub(gs.CreationTimestamp.Time).Milliseconds())
+				recentStandingByCount++
+			}
 		} else if gs.Status.State == mpsv1alpha1.GameServerStateActive && gs.Status.Health == mpsv1alpha1.GameServerHealthy {
 			activeCount++
 		} else if gs.Status.State == mpsv1alpha1.GameServerStateGameCompleted && gs.Status.Health == mpsv1alpha1.GameServerHealthy {
@@ -212,9 +212,9 @@ func (r *GameServerBuildReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		}
 	}
 
-	// if recentStandingByCount > 0 {
-	// 	GameServersCreatedDuration.WithLabelValues(gsb.Name).Set(timeToStandBySum / float64(recentStandingByCount))
-	// }
+	if recentStandingByCount > 0 {
+		GameServersCreatedDuration.WithLabelValues(gsb.Name).Set(timeToStandBySum / float64(recentStandingByCount))
+	}
 
 	if pendingCleanUpCount > 0 {
 		GameServersCleanUpDuration.WithLabelValues(gsb.Name).Set(timeToDeleteBySum / float64(pendingCleanUpCount))
